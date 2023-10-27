6762d41f0958ea7580a0422ad1235c35a8326972
duck <cmkyduck1ove@naver.com>
2023-10-27 14:28:58 +0900

    231026
    OJT #2 3차 업로드

--- OJT_2/main.cpp
+++ OJT_2/main.cpp
@@ -7,25 +7,22 @@
 
 #define SOURCE_WIDTH 4
 #define SOURCE_HEIGHT 8
-#define TYPE_NUM 1 // 1 이외의 숫자를 입력하면 'ㄱ' 그림 배열로 변경
+#define TYPE_NUM 2 // 1 이외의 숫자를 입력하면 'ㄱ' 그림 배열로 변경
 
-int SOURCET_BUF[SOURCE_WIDTH*SOURCE_HEIGHT];
+int SOURCET_BUF[SOURCE_WIDTH*SOURCE_HEIGHT]; // source buffer
 
 // 왼쪽으로 회전
 void rotate_left() {
 
-    for(int i = 0; i < SOURCE_WIDTH; i++) {
-		// 원본 배열의 첫번째 인덱스부터 순서대로 출력 SOURCET_BUF[0]
-        for(int j = 0; j < SOURCE_WIDTH*SOURCE_HEIGHT; j++) {
-			// 왼쪽으로 회전한 첫번째 행의 인덱스는 SOURCE_WIDTH의 크기로 나누었을때 나머지가 SOURCE_WIDTH-1
-            if(j % SOURCE_WIDTH == SOURCE_WIDTH-1) {
-                std::cout << std::setw(2) << std::setfill('0'); // 숫자가 한자리 숫자라면 공백에 0을 부여
-                std::cout << SOURCET_BUF[j-i];
-                // 현재 인덱스가 SOURCE_WIDTH*SOURCE_HEIGHT-SOURCE_WIDTH의 크기보다 크거나 같을 경우 다음 행으로 넘어감
-                if(j-i >= SOURCE_WIDTH*SOURCE_HEIGHT-SOURCE_WIDTH){
-                    std::cout << "\n";
-                }
-            }
+	for(int i = 0, j = 0, k = 0; i < SOURCE_WIDTH*SOURCE_HEIGHT; i++) {
+		std::cout << std::setw(2) << std::setfill('0'); // 숫자가 한자리 숫자라면 공백에 0을 부여
+        std::cout << SOURCET_BUF[SOURCE_WIDTH*(k+1)-1-j];
+        k++;
+        // 현재 행의 크기가 SOURCE_HEIGHT의 크기와 같아지면 다음 행으로 넘어감
+        if(k == SOURCE_HEIGHT) {
+            std::cout << "\n";
+            j++;
+            k = 0;
         }
     }
 }
@@ -33,18 +30,15 @@ void rotate_left() {
 // 오른쪽으로 회전
 void rotate_right() {
 
-	for(int i = 0; i < SOURCE_WIDTH; i++) {
-		// 원본 배열의 마지막 인덱스부터 순서대로 출력 SOURCET_BUF[SOURCE_WIDTH*SOURCE_HEIGHT-1]
-        for(int j = SOURCE_WIDTH*SOURCE_HEIGHT - 1 ; j >= 0; j--) {
-        	// 오른쪽으로 회전한 첫번째 행의 인덱스는 SOURCE_WIDTH의 배수
-            if(j % SOURCE_WIDTH == 0) {
-				std::cout << std::setw(2) << std::setfill('0'); // 숫자가 한자리 숫자라면 공백에 0을 부여
-                std::cout << SOURCET_BUF[j+i];
-                // 현재 인덱스가 SOURCE_WIDTH의 크기보다 작을 경우 다음 행으로 넘어감
-				if(j+i < SOURCE_WIDTH) {
-					std::cout << "\n";
-                }
-            }
+	for(int i = SOURCE_WIDTH*SOURCE_HEIGHT, j = 0, k = SOURCE_HEIGHT - 1; i > 0; i--) {
+		std::cout << std::setw(2) << std::setfill('0'); // 숫자가 한자리 숫자라면 공백에 0을 부여
+        std::cout << SOURCET_BUF[(SOURCE_WIDTH*k)+j];
+        k--;
+        // 현재 행의 크기가 SOURCE_HEIGHT의 크기와 같아지면 다음 행으로 넘어감
+        if(k + 1 == 0) {
+            std::cout << "\n";
+            j++;
+            k = SOURCE_HEIGHT - 1;
         }
     }
 }
@@ -67,13 +61,13 @@ void rotate_reverse() {
 int main() {
 
     // 매크로 상수 TYPE_NUM이 1이라면 숫자 데이터 부여
-	if (TYPE_NUM == 1){
+	#if TYPE_NUM == 1
         for(int i = 0; i < SOURCE_WIDTH*SOURCE_HEIGHT; i++) {
             SOURCET_BUF[i] = i+1;
         }
-	}
+
 	// 매크로 상수 TYPE_NUM이 1이 아니라면 'ㄱ' 그림 부여
-	else {
+	# else
         for(int i = 0; i < SOURCE_WIDTH*SOURCE_HEIGHT; i++) {
             if(i < SOURCE_WIDTH || (i%SOURCE_WIDTH)+1 == SOURCE_WIDTH) {
                 // 배열의 첫번째 행과 현재 행의 마지막 열에 속하는 경우 '11'을 부여
@@ -84,7 +78,8 @@ int main() {
                 SOURCET_BUF[i] = 0;
             }
         }
-	}
+
+    #endif // TYPE_NUM
 
     std::cout << "기본출력" << std::endl;
 
