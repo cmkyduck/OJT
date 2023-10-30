7502aa0ad3c297fe61c8573d7a01fb708dbfd77e
duck <cmkyduck1ove@naver.com>
2023-10-30 15:53:47 +0900

    231030
    OJT #4 1차 업로드

--- OJT_4/main.cpp
+++ OJT_4/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <string>
-#include <sstream>  // stringstream 라이브러리
-#include <iomanip>  // fixed 라이브러리
-#include <cmath>  // setprecision 라이브러리
+#include <sstream>
 
 using namespace std;
 
@@ -24,38 +22,51 @@ public:
     double getResult() { calculate(); return result; }
 };
 
-// 자식 클래스 : 더하기
 class Add : public Operator{
     // write
     virtual void calculate(){
-        cout << getNum1() << " + " << getNum2() << " = " << getNum1() + getNum2() << '\n' << '\n';
+        setResult(getNum1() + getNum2());
     }
 };
 
-// 자식 클래스 : 빼기
 class Subtract : public Operator{
     // write
     virtual void calculate(){
-        cout << getNum1() << " - " << getNum2() << " = " << getNum1() - getNum2() << '\n' << '\n';
+        setResult(getNum1() - getNum2());
     }
 };
 
-// 자식 클래스 : 곱하기
 class Multiply : public Operator{
     // write
     virtual void calculate(){
-        cout << getNum1() << " * " << getNum2() << " = " << fixed << setprecision(0) << (double)getNum1() * (double)getNum2() << '\n' << '\n';
+        setResult(getNum1() * getNum2());
     }
 };
 
-// 자식 클래스 : 나누기
 class Divide : public Operator{
     // write
-
     virtual void calculate(){
-        cout << getNum1() << " / " << getNum2() << " = " << fixed << setprecision(8) << (double)getNum1() / (double)getNum2() << '\n' << '\n';
+        // int를 double로 형변환
+        setResult(static_cast<double>(getNum1()) / getNum2());
     }
 };
+
+// 입력된 연산자가 +, -, *, /인 경우 true를 반환하는 함수
+bool check_sign(char sign) {
+    return (sign == '+' || sign == '-' || sign == '*' || sign == '/');
+}
+
+// 잘못된 연산자를 입력하면 true를 반환하는 함수
+bool forbid_sign(const string& str) {
+    string forbid_sign[] = {"++", "**", "//", "-+", "*+", "/+"};
+    // 범위 기반 for 루프로 배열의 모든 요소를 출력
+    for (const string& pattern : forbid_sign) {
+        if (str.find(pattern) != string::npos) {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main()
 {
@@ -74,98 +85,70 @@ int main()
 
         // write
 
-        // stringstream : 문자열에서 필요한 정보만 추출
-        stringstream stream(input_exp);
-        stream >> num1;
-        stream >> sign;
-        stream >> num2;
-
-        int len = input_exp.length();  // 입력받은 문자열 길이 구하기
-        int sign_index = input_exp.find(sign);  // sign 인덱스
-        int sign_subtract = input_exp.find('-',sign_index+2);  // 예외 상황 중 '-'제외에 활용할 '-' 인덱스
-
-        // 1. "break" 입력 시, 프로그램 종료
-        if(input_exp == "break"){
+        // break 입력 시 종료
+        if (input_exp == "break") {
             break;
         }
 
-        // 2. 입력한 문자열이 길이가 12 이하이면서 연산자는 +,-,*,/ 인지 판단
-        if((len > 12) || (sign != '+') && (sign != '-') && (sign != '*') && (sign != '/')){
-            cout << '\n' << "올바르게 입력했는지 확인하세요." << '\n';
+        // #1 예외 처리 : 첫번째와 마지막 연산자에 '+'가 입력된 경우
+        if (input_exp[input_exp.length() - 1] == '+' || input_exp[0] == '+') {
+            cerr << "오류: 올바르게 입력했는지 확인하세요.\n";
             cout << "===============================" << '\n' << '\n';
             continue;
         }
 
-        char ch[] = {'+', '*', '/'};  // 입력된 연산자와 비교하기 위한 연산자 배열 선언
-        char sign_arr;
+        // #2 예외 처리 : 연산자를 잘못 사용해서 쓴 경우
+        if (forbid_sign(input_exp)) {
+            cerr << "오류: 잘못된 연산자를 사용했습니다. 올바르게 입력하세요." << endl;
+            cout << "===============================" << '\n' << '\n';
+            continue;
+        }
 
-        // '-'를 제외한 기호들을 for문을 활용해 하나씩 추출해 3, 4번에 적용
-        for(int i = 0; i < 3; i++) {
+        istringstream iss(input_exp);
 
-            sign_arr = ch[i];
+        if (iss >> num1 >> sign >> num2) {
 
-            int sign_temp = input_exp.find(sign_arr,sign_index+1);  // num2 이후에 또 sign이 입력되면, 그 sign의 인덱스를 저장
-            int first = input_exp.find_first_of(sign_arr,0);  // 입력된 연산자 중 가장 앞 쪽에 놓인 sign의 인덱스
-            int last = input_exp.find_last_of(sign_arr,len-1);  // 입력된 연산자 중 가장 뒤 쪽에 놓인 sign의 인덱스
+            // #3 예외 처리 : '+, -, *, /'를 제외한 연산자가 입력된 경우
+            if (!check_sign(sign) || iss.rdbuf() -> in_avail() > 0) {
+                cerr << "오류: 올바른 연산자를 입력했는지 확인하세요.\n";
+                cout << "===============================" << '\n' << '\n';
+                continue;
+            }
 
-            // 3. 앞에서부터 찾은 연산자 인덱스,뒤에서부터 찾은 연산자 인덱스 비교
-            if(first == last){
+            // #4 예외 처리 : 0으로 나누는 경우
+            if (sign == '/' && num2 == 0) {
+                cerr << "오류: 0으로 나눌 수 없습니다.\n";
+                cout << "===============================" << '\n' << '\n';
+                continue;
+            }
 
-                // 4. '-'를 맨 앞 혹은 num2 앞에 입력했는지 판단
-                if(input_exp.find('-') == 0 || input_exp.find('-') == sign_index+1) {
+            Operator* op; // Operator 클래스의 포인터 변수 op를 선언
+
+            // 입력된 연산자에 따라 해당하는 Operator 객체를 선택
+            switch (sign) {
+                case '+':
+                    op = &a;
                     break;
-                }
+                case '-':
+                    op = &s;
+                    break;
+                case '*':
+                    op = &m;
+                    break;
+                case '/':
+                    op = &d;
+                    break;
             }
-            else{
-                cout << '\n' << "계산식을 올바르게 입력했는지 확인하세요." << '\n';
-                cout << "========================================" << '\n' << '\n';
-                sign = {};  // sign 초기화
-                break;
+            // 선택된 Operator 객체에 피연산자를 설정
+            op -> setNumber(num1, num2);
+            double result = op -> getResult();
 
-            }
-
-            // 5. 입력 정수가 3개 이상인지 판단
-            if(sign_temp != string::npos || sign_subtract != string::npos) {
-                cout << '\n' << "서로 다른 연산자를 2개 이상 입력했는지 확인하세요.('-'제외)" << '\n';
-                cout << "===========================================================" << '\n' << '\n';
-                sign = {};  // sign 초기화
-                break;
-            }
+            cout << "[ 결과 ] : " << result << endl;
+        // 입력이 올바르지 않은 경우 오류 메시지 출력
+        } else {
+            cerr << "오류: 유효하지 않은 입력 형식입니다." << endl;
+            cout << "===============================" << '\n' << '\n';
         }
-
-        switch(sign) {
-            case '+':
-                cout << '\n' << "[출력]" << '\n';
-                a.setNumber(num1, num2);
-                a.getResult();
-                break;
-            case '-':
-                cout << '\n' << "[출력]" << '\n';
-                s.setNumber(num1, num2);
-                s.getResult();
-                break;
-            case '*':
-                cout << '\n' << "[출력]" << '\n';
-                m.setNumber(num1, num2);
-                m.getResult();
-                break;
-            case '/':
-                if(num1 != 0 && num2 != 0) {
-                    cout << '\n' << "[출력]" << '\n';
-                    d.setNumber(num1, num2);
-                    d.getResult();
-                }
-                else {
-                    cout << '\n' << "분자 혹은 분모에 '0'을 입력했는지 확인하세요." << '\n';
-                    cout << "=============================================" << '\n' << '\n';
-                }
-                break;
-            default :
-                cout << "다시 시도하세요." << '\n' << '\n';
-                break;
-        }
-        num1, num2 = 0;  // num1, num2 초기화
-        sign = {};  // 연산기호 초기화
     }
     return 0;
 }
